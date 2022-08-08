91ebf1cee10b490a81d2b4e4a733cd4b3c3f608d
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 11:57:23 +0200

    Add simple transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animateTheBlock }"></div>
+    <button @click="animatedBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,9 +15,12 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { dialogIsVisible: false, animateTheBlock: false };
   },
   methods: {
+    animatedBlock() {
+      this.animateTheBlock = true;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -57,6 +60,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 0.6s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +72,8 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  transform: translateX(-150px);
 }
 </style>
