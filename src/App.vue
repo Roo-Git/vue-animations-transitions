f89d374330d1331af952278751053a3d76960a56
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 13:05:44 +0200

    add transition to btn

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -30,9 +36,16 @@ export default {
       dialogIsVisible: false,
       animateTheBlock: false,
       paragraphIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
     toggleParagraph() {
       this.paragraphIsVisible = !this.paragraphIsVisible;
     },
@@ -118,6 +131,24 @@ button:active {
 .paragraph-leave-to {
   opacity: 0;
   transform: translateY(30px);
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 @keyframes slide-fade {
